8c2b51f15b46a930c2724d72e7389c9e34aee4d5
Renan Dantas <renan.dantas3@etec.sp.gov.br>
2025-02-18 19:42:29 -0300

    Projeto NSA 2.0 terminado

--- nsa-2/app/index.tsx
+++ nsa-2/app/index.tsx
@@ -1,14 +1,50 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
-export default function Index() {
+export default function Login() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-      }}
-    >
-      <Text>qlqr coisa</Text>
+    <View style = {estilos.pagina}>
+      <Text style = {estilos.titulo}>Login</Text>
+
+        <TextInput placeholder="Usuário" style = {estilos.texto} />
+        <TextInput placeholder="Senha" style = {estilos.texto} />
+        
+        <Link href = "/home" asChild>
+          <TouchableOpacity style = {estilos.botao}>
+            <Text style = {{ color: "white" }}>Entrar</Text>
+          </TouchableOpacity>
+        </Link>
     </View>
   );
 }
+
+const estilos = StyleSheet.create ({
+  pagina: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  titulo: {
+    fontSize: 32,
+    marginBottom: 20,
+  },
+
+  texto: {
+    width: 250,
+    height: 45,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    fontSize: 16,
+  },
+
+  botao: {
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+    marginTop: 20,
+    backgroundColor: "gray",
+  }
+})
